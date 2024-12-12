d24c9ed4b8ceef0e9656c7da48fc27dc654935be
Hamza <muhammadhamzam1486@gmail.com>
2024-12-12 21:57:40 +0500

    Enhance SMOTE resampling function with customizable random state and sampling strategy

--- utilities/resample_with_smote.py
+++ utilities/resample_with_smote.py
@@ -2,8 +2,10 @@ import pandas as pd
 from imblearn.over_sampling import SMOTE
 
 
-def resample_with_smote(data: pd.DataFrame) -> pd.DataFrame:
-    smote = SMOTE(sampling_strategy="minority")
+def resample_with_smote(
+    data: pd.DataFrame, random_state=None, sampling_strategy: str | float = 0.5
+) -> pd.DataFrame:
+    smote = SMOTE(sampling_strategy=sampling_strategy, random_state=random_state)
     X, y = smote.fit_resample(data.drop("class", axis=1), data["class"])
     X["class"] = y
     return X
