4d6081c4c87e48bbe4e93cfbc778a95ed631dc6b
Hamza <muhammadhamzam1486@gmail.com>
2024-12-12 22:18:34 +0500

    Rename draw_subplots function to draw_bar_class_value_counts for clarity and specificity

--- plots/bar_plot.py
+++ plots/bar_plot.py
@@ -3,7 +3,7 @@ import pandas as pd
 from typing import List
 
 
-def draw_subplots(datasets: List[pd.DataFrame], titles: List[str]):
+def draw_bar_class_value_counts(datasets: List[pd.DataFrame], titles: List[str]):
     fig, axs = plt.subplots(1, len(datasets), figsize=(15, 5))
     for i, data in enumerate(datasets):
         data["class"].value_counts().plot(kind="bar", ax=axs[i])
